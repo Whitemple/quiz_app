c84224e9fa72b3ef80f230def9e8b3cbf6f7b7e7
whitemple <yarikkhromey@mail.ru>
2024-11-12 18:21:18 +0300

    created basic markup

---
+++ src/js/questions.js
@@ -0,0 +1,76 @@
+const myQuestions = [
+    {
+        id: 1,
+        question: 'What is the capital of France?', 
+        answers: [ 
+            { 
+                text: 'Paris', 
+                correct: true 
+            }, 
+            { 
+                text: 'London', 
+                correct: false 
+            }, 
+            {
+                text: 'Berlin', 
+                correct: false 
+            }, 
+            { 
+                text: 'Madrid', 
+                correct: false 
+            } 
+        ] 
+    },
+    {
+        id: 2,
+        question: 'Who invented JavaScript?', 
+        answers: [ 
+            { 
+                text: 'Douglas Crockford', 
+                correct: false 
+            }, 
+            { 
+                text: 'Sheryl Sandberg', 
+                correct: false 
+            }, 
+            {
+                text: 'Brendan Eich', 
+                correct: true 
+            }, 
+            { 
+                text: 'Sandberg Crockford', 
+                correct: false 
+            } 
+        ] 
+    },
+        // {
+    //     id: 1,
+    //     question: "Who invented JavaScript?",
+    //     answers: {
+    //     a: "Douglas Crockford",
+    //     b: "Sheryl Sandberg",
+    //     c: "Brendan Eich"
+    //     },
+    //     correctAnswer: "c"
+    // },
+    // {
+    //     id: 2,
+    //     question: "Who invented JavaScript?",
+    //     answers: {
+    //         a: "Douglas Crockford",
+    //         b: "Sheryl Sandberg",
+    //         c: "Brendan Eich"
+    //     },
+    //     correctAnswer: "a"
+    // },
+    // {
+    //     id: 3,
+    //     question: "Who invented JavaScript?",
+    //     answers: {
+    //         a: "Douglas Crockford",
+    //         b: "Sheryl Sandberg",
+    //         c: "Brendan Eich"
+    //     },
+    //     correctAnswer: "b"
+    // },
+]
